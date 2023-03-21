2a4a610aecf6a163f63fb5110839103d2ed599ce
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2023-03-20 23:09:01 -0300

    changing default layout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,12 @@
+import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header";
-import { Home } from "../../pages/Home";
 import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
     return (
         <LayoutContainer>
             <Header />
-            <Home />
+            <Outlet />
         </LayoutContainer>
     )
 }
