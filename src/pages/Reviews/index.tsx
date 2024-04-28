ab179d613eca6c69ad65ca04aed47bd225d67934
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2024-04-28 12:08:43 -0300

    updated reviews page

--- src/pages/Reviews/index.tsx
+++ src/pages/Reviews/index.tsx
@@ -53,7 +53,6 @@ export function Review(){
                 '/api'
             );
             const json = await response.json();
-            console.log({json});
             setReviewsSort(json.result.reviews);
         } catch (error) {
             console.error(error);
