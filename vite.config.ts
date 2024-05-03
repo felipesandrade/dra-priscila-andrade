a14c351f838a5c31abf5361147c0f8d457a3e235
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2024-05-03 17:29:59 -0300

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig (({ command, mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    server: {
-      proxy: {
-        "/api": {
-          ws: true,
-          changeOrigin: true,
-          secure: false,
-          target: env.VITE_GOOGLE_MAPS_REVIEWS_URL,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        }, 
-      },
-    },
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
 })
