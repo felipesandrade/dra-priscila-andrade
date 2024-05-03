bfb7978b8e729adb0d738a640b70149182312395
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2024-05-03 17:45:36 -0300

    added vite plugin verce

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig (({ command, mode }) => {
@@ -18,6 +19,6 @@ export default defineConfig (({ command, mode }) => {
         }, 
       },
     },
-    plugins: [react()],
+    plugins: [react(), vercel()],
   };
 })
