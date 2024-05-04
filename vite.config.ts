8d7445643ae7c9e86233d2e4e93e17ac7db2ad06
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2024-05-04 12:29:59 -0300

    updated vercel json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig (({ command, mode }) => {
           rewrite: (path) => path.replace(/^\/api/, ''),
         }, 
       },
+      cors: false,
     },
     plugins: [react(), vercel()],
   };
